69e5a992bb16ef99c4c5425768aaed2bc0aeb5c0
¨Venturin¨ <diegoventurinn@gmail.com>
2022-12-10 22:13:11 -0300

    chore: alterada as pastas para produção na azure

---
+++ Migrations/20221210222819_AddContentMaxLength.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tryitter.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContentMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+        }
+    }
+}
