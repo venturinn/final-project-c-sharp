3a0197d6c12b1be7c16dab0409e1c73e97ae3c86
¨Venturin¨ <diegoventurinn@gmail.com>
2022-12-11 16:53:46 -0300

    test: implementados testes de integração da rota PUT /UserLogin

--- src/tryitter.Test/TryitterTest.cs
+++ src/tryitter.Test/TryitterTest.cs
@@ -1,4 +1,3 @@
-using System.Text.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using tryitter.Models;
@@ -11,7 +10,6 @@ using tryitter.Test;
 using tryitter.Services;
 using System.Net.Http.Headers;
 
-using System.Text.Json.Nodes;
 using System.Net.Http.Json;
 
 namespace SignInTest.Test;
@@ -196,7 +194,7 @@ public class TryitterTest : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory(DisplayName = "POST /UserLogin deve retornar status BadRequest se o post tiver for > 300 caracteres")]
-    [MemberData(nameof(UserLoginShouldReturnReturnBadRequestEntries))]
+    [MemberData(nameof(UserLoginShouldReturnBadRequestEntries))]
     public async Task UserLoginShouldReturnReturnBadRequest(UserLogin rightUserLogin, PostUser post)
     {
         // Realizando o login para receber JWT Token
@@ -216,14 +214,27 @@ public class TryitterTest : IClassFixture<WebApplicationFactory<Program>>
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
 
+    // Altera os dados cadastrais da conta do usuário logado
+    [Theory(DisplayName = "PUT /UserLogin deve alterar o registro o usuário logado")]
+    [MemberData(nameof(UserLoginShouldReturnReturnOkEntries))]
+    public async Task UserLoginShouldReturnReturnOk(UserLogin rightUserLogin, User newUserData)
+    {
+        // Realizando o login para receber JWT Token
+        var json = JsonConvert.SerializeObject(rightUserLogin);
+        StringContent content = new(json, Encoding.UTF8, "application/json");
 
+        var responseSignIn = await client.PostAsync("SignIn", content);
+        var token = await responseSignIn.Content.ReadFromJsonAsync<Token>();
 
+        // Requisição para o endpoint a ser testado
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.TokenMessage);
+        var jsonUser = JsonConvert.SerializeObject(newUserData);
+        StringContent contentUser = new(jsonUser, Encoding.UTF8, "application/json");
 
+        var response = await client.PutAsync("UserLogin", contentUser);
 
-
-
-
-
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
 
     public static readonly TheoryData<UserLogin> rightUserLogin = new()
     {
@@ -306,11 +317,19 @@ public class TryitterTest : IClassFixture<WebApplicationFactory<Program>>
         }
     };
 
-    public static readonly TheoryData<UserLogin, PostUser> UserLoginShouldReturnReturnBadRequestEntries = new()
+    public static readonly TheoryData<UserLogin, PostUser> UserLoginShouldReturnBadRequestEntries = new()
     {
         {
             new UserLogin { Email = "one@email.com",Password = "one.123"},
             new PostUser { Content = "Sed autem suscipit et quisquam rerum et dolore eaque ab perferendis quisquam qui ipsam soluta aut odio minima ad sequi voluptas. Sed quisquam quidem ad maiores tempore et atque pariatur qui autem sunt qui nihil quia aut fugiat quidem. Rem vero minima quo cupiditate magni est assumenda facilis in fugit nemo." }
+        }
+    };
+
+    public static readonly TheoryData<UserLogin, User> UserLoginShouldReturnReturnOkEntries = new()
+    {
+        {
+            new UserLogin { Email = "one@email.com",Password = "one.123"},
+            new User { Name = "User 5", Email = "five@email.com", Module = "Front-end", Status = "Concluído", Password = "five.123"}
         }
     };
 }
